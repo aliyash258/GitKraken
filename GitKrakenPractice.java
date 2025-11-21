5a44312cd6b9edb6e64a5d33b532b73997950fcb
aliyash258 <ashaikh42308@gmail.com>
2025-11-21 13:50:00 -0500

    I stored all sums from each thread.

--- GitKrakenPractice.java
+++ GitKrakenPractice.java
@@ -4,16 +4,28 @@ public class GitKrakenPractice
 {
 	public static void main(String[] args)
 	{
+		int[] totals = new int[1000];
 		MyThread[] threads = new MyThread[1000];
+		for(int i = 0; i < threads.length; i++)
+		{
+			threads[i] = new MyThread();
+			totals[i] = threads[i].getSum();
+		}
 	}
 }
 
 class MyThread implements Runnable
 {
+	private int sum;
+
 	public void run()
 	{
-		int sum = 0;
 		for(int i = 1; i <= 1000000; i++)
 			sum += i;
+	}
+
+	public int getSum()
+	{
+		return sum;
 	}
 }
