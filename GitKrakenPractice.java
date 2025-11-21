a9da3e50ea9a0ccad5e8c6499db8374794175d99
aliyash258 <ashaikh42308@gmail.com>
2025-11-21 13:39:13 -0500

    I created 1000 threads that are stored in an array.

---
+++ GitKrakenPractice.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class GitKrakenPractice
+{
+	public static void main(String[] args)
+	{
+		MyThread[] threads = new MyThread[1000];
+	}
+}
+
+class MyThread implements Runnable
+{
+	public void run()
+	{
+
+	}
+}
